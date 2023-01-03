a2c1d55f2ed7cb986cc19c36ef0cd39dca871713
sudmantri <sudhanshusingh787@gmail.com>
2023-01-03 16:14:45 +0530

    db changes

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ import AddCommentOutlinedIcon from '@mui/icons-material/AddCommentOutlined';
 export const api = {
   //host: "http://localhost:8000/api/",
   // host: 'http://44.202.123.160:8000/api/'
-  host: 'https://growlock-backend.onrender.com/api/'
-  //  host: "http://localhost:4000/api/",
+ // host: 'https://growlock-backend.onrender.com/api/'
+    host: "http://localhost:4000/api/",
   // host: 'http://44.202.123.160:8000/api/'
   // host:'https://growlock-backend.onrender.com/api/'
 };
