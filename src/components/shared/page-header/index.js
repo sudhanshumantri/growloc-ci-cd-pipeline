8078c840e5c99c30131d673fce582abef7c5879f
Kishore-konamalla <kishore_konamalla@avocadolabs.in>
2023-05-18 20:19:59 +0530

    page-header

--- src/components/shared/page-header/index.js
+++ src/components/shared/page-header/index.js
@@ -1,4 +1,4 @@
-import React,{useState,useRef,useEffect}from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Button from "../button";
 import {
   Grid,
@@ -11,12 +11,12 @@ import {
   Card,
   CardContent,
   Typography,
-  Tooltip
+  Tooltip,
 } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "./style.css";
 import AuthOutlet from "../authoutlet";
-import InfoIcon from '@mui/icons-material/Info';
+import InfoIcon from "@mui/icons-material/Info";
 
 function renderButtonArray(buttonArray) {
   return buttonArray.map((item, index) => {
@@ -35,9 +35,7 @@ function renderButtonArray(buttonArray) {
         />
       </AuthOutlet>
     );
-
   });
-
 }
 function renderSelectBox() {
   return <Select />;
@@ -58,7 +56,6 @@ function renderBackButtonArray(showBackButton) {
     );
   });
 }
-
 export default function PageHeader({
   title,
   subtitle,
@@ -74,109 +71,142 @@ export default function PageHeader({
   handleChange,
 }) {
   const { length } = options || "";
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const [tooltipTitle, setTooltipTitle] = useState("");
+
+  const handleTitleClick = (e, name) => {
+    setTooltipOpen(true);
+    setTooltipTitle(name);
+  };
+  const handleTooltipClose = () => {
+    setTooltipOpen(false);
+    setTooltipTitle("");
+  };
+  const handleTitleMouseOver = (e, name) => {
+    setTooltipOpen(true);
+    setTooltipTitle(name);
+  };
+
   return (
     <>
       {showBackButton && showBackButton.length > 0 ? (
-    <Grid container spacing={2} className="page-header-container ">
-      {/* <Grid item xs={1} sm={1} md={1}>
-        {renderBackButtonArray(showBackButton)}
-      </Grid> */}
-      <Grid item xs={4} sm={4} md={4} sx={{display:"flex",}}>
-      {renderBackButtonArray(showBackButton)}
-        <p className="page-section-title">
-          {title}
-          {subtitle && <span className="label-light">{subtitle}</span>}
-        </p>
-      </Grid>
-      <Grid item xs={8} sm={8} md={8} className="button-container">
-        {headerDropwdown && (
-          <Select
-            value={length ? value || "" : ""}
-            size="small"
-            onChange={handleChange}
-          >
-            {(options || []).map((option, index) => (
-              <MenuItem
-                value={valuekey ? option[valuekey] || index : option}
-                key={index}
-                className="menu-item-dropdown-item"
+        <Grid container spacing={2} className="page-header-container ">
+          <Grid item xs={1} sm={1} md={1}>
+            {renderBackButtonArray(showBackButton)}
+          </Grid>
+          <Grid item xs={4} sm={4} md={4} sx={{ marginLeft: "-5%",
+                      
+        }}>
+            <Grid item xs={12} sm={12} md={12}>
+              <Tooltip
+                title={tooltipTitle}
+                open={tooltipOpen}
+                onClose={handleTooltipClose}
+                enterDelay={500}
               >
-                {labelkey ? option[labelkey] : option}
-              </MenuItem>
-            ))}
-          </Select>
-        )}
-
-        {buttonArray &&
-          buttonArray.length > 0 &&
-          renderButtonArray(buttonArray)}
-      </Grid>
-      {info && (
-        <Grid item xs={12} sm={12} md={12}>
-          {info.map((data, index) => (
-            <span className="label-light" key={index}>
-              {data.title && (
-                <>
-                  <b>{data.title}</b>:
-                </>
-              )}
-              {data.value}
-              {info.length - 1 !== index && <>, </>}
-            </span>
-          ))}
-        </Grid>
-      )}
-    </Grid>
-  ) : (
-    <Grid container spacing={2} className="page-header-container ">
-      <Grid item xs={6} sm={6} md={3}>
-        <p className="page-section-title">
-          {title}
-          {subtitle && <span className="label-light">{subtitle}</span>}
-        </p>
-      </Grid>
-      <Grid item xs={6} sm={5} md={9} className="button-container">
-        {headerDropwdown && (
-          <Select
-            value={length ? value || "" : ""}
-            size="small"
-            onChange={handleChange}
-          >
-            {(options || []).map((option, index) => {
-              return (
-                <MenuItem
-                  value={valuekey ? option[valuekey] || index : option}
-                  key={index}
-                  className="menu-item-dropdown-item"
+                <p
+                  className="page-section-title"
+                  onClick={(e) => handleTitleClick(e, title)}
+                  onMouseOver={(e) => handleTitleMouseOver(e, title)}
+                  onMouseLeave={handleTooltipClose}
                 >
-                  {labelkey ? option[labelkey] : option}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        )}
-        {buttonArray &&
-          buttonArray.length > 0 &&
-          renderButtonArray(buttonArray)}
-      </Grid>
-      {info && (
-        <Grid item xs={12} sm={12} md={12}>
-          {info.map((data, index) => (
-            <span className="label-light" key={index}>
-              {data.title && (
-                <>
-                  <b>{data.title}</b>:
-                </>
-              )}
-              {data.value}
-              {info.length - 1 != index && <>, </>}
-            </span>
-          ))}
+                  {title}
+                </p>
+              </Tooltip>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              {subtitle && <span className="label-light">{subtitle}</span>}
+            </Grid>
+          </Grid>
+          <Grid item xs={7} sm={7} md={7} className="button-container"  >
+            {headerDropwdown && (
+              <Select
+                value={length ? value || "" : ""}
+                size="small"
+                onChange={handleChange}
+              >
+                {(options || []).map((option, index) => (
+                  <MenuItem
+                    value={valuekey ? option[valuekey] || index : option}
+                    key={index}
+                    className="menu-item-dropdown-item"
+                  >
+                    {labelkey ? option[labelkey] : option}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+
+            {buttonArray &&
+              buttonArray.length > 0 &&
+              renderButtonArray(buttonArray)}
+          </Grid>
+          {info && (
+            <Grid item xs={12} sm={12} md={12}>
+              {info.map((data, index) => (
+                <span className="label-light" key={index}>
+                  {data.title && (
+                    <>
+                      <b>{data.title}</b>:
+                    </>
+                  )}
+                  {data.value}
+                  {info.length - 1 !== index && <>, </>}
+                </span>
+              ))}
+            </Grid>
+          )}
+        </Grid>
+      ) : (
+        <Grid container spacing={2} className="page-header-container ">
+          <Grid item xs={6} sm={6} md={3}>
+            <p className="page-section-name">
+              {title}
+              {subtitle && <span className="label-light">{subtitle}</span>}
+            </p>
+          </Grid>
+          <Grid item xs={6} sm={5} md={9} className="button-container">
+            {headerDropwdown && (
+              <Select
+                value={length ? value || "" : ""}
+                size="small"
+                onChange={handleChange}
+              >
+                {(options || []).map((option, index) => {
+                  return (
+                    <MenuItem
+                      value={valuekey ? option[valuekey] || index : option}
+                      key={index}
+                      className="menu-item-dropdown-item"
+                    >
+                      {labelkey ? option[labelkey] : option}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            )}
+            {buttonArray &&
+              buttonArray.length > 0 &&
+              renderButtonArray(buttonArray)}
+          </Grid>
+          {info && (
+            <Grid item xs={12} sm={12} md={12}>
+              {info.map((data, index) => (
+                <span className="label-light" key={index}>
+                  {data.title && (
+                    <>
+                      <b>{data.title}</b>:
+                    </>
+                  )}
+                  {data.value}
+                  {info.length - 1 != index && <>, </>}
+                </span>
+              ))}
+            </Grid>
+          )}
         </Grid>
       )}
-    </Grid>
-  )}
-  <Divider />
+      <Divider />
     </>
   );
 }
